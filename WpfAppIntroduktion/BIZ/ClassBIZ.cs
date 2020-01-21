099465f9401b45d123e4ef972d8c08410b8a8aa6
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-01-21 11:54:09 +0100

    Add project files.

---
+++ WpfAppIntroduktion/BIZ/ClassBIZ.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfAppIntroduktion.BIZ
+{
+    public class ClassBIZ
+    {
+        public ClassBIZ()
+        {
+
+        }
+
+        public void Delopgave12(ListBox listBox)
+        {
+            ClearList(listBox);
+            int number = 4711;
+            while (number >= 4711 && number <= 4736)
+            {
+                listBox.Items.Add(number.ToString());
+                number++;
+            }
+        }
+
+        public void Delopgave13(ListBox listBox)
+        {
+            ClearList(listBox);
+            Random random = new Random();
+            for (int i = 0; i < 25; i++)
+            {
+                listBox.Items.Add(random.Next(100000, 1000001));
+            }
+        }
+
+        public void Delopgave14(ListBox listBox)
+        {
+            ClearList(listBox);
+            Random random = new Random();
+            List<int> listOfNumbers = new List<int>();
+            for (int i = 0; i < 25; i++)
+            {
+                listOfNumbers.Add(random.Next(100000, 1000001));
+            }
+            listOfNumbers.Sort();
+            foreach (int number in listOfNumbers)
+            {
+                listBox.Items.Add(number);
+            }
+        }
+
+        public void Delopgave15(ListBox listBox)
+        {
+            ClearList(listBox);
+            Random random = new Random();
+            List<int> listOfNumbers = new List<int>();
+            for (int i = 0; i < 25; i++)
+            {
+                listOfNumbers.Add(random.Next(100000, 1000001));
+            }
+            List<int> listOfNumbersSorted = new List<int>(listOfNumbers);
+            listOfNumbersSorted.Sort();
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                listBox.Items.Add(listOfNumbers[i] + "\t" + listOfNumbersSorted[i]);
+            }
+        }
+
+        public void Delopgave16(ListBox listBox)
+        {
+            ClearList(listBox);
+            Random random = new Random();
+            List<int> listOfNumbers = new List<int>();
+            for (int i = 0; i < 25; i++)
+            {
+                int number = random.Next(100000, 1000001);
+                listOfNumbers.Add(number);
+                listBox.Items.Add(number);
+            }
+            listBox.Items.Add("");
+            listBox.Items.Add("Gennemsnitsværdi: " + GetAverage(listOfNumbers));
+        }
+
+        public void Delopgave17(ListBox listBox)
+        {
+            ClearList(listBox);
+            Random random = new Random();
+            List<int> listOfNumbers = new List<int>();
+            for (int i = 0; i < 25; i++)
+            {
+                listOfNumbers.Add(random.Next(100000, 1000001));
+            }
+            int average = GetAverage(listOfNumbers);
+            listOfNumbers.Sort();
+            foreach (int number in listOfNumbers)
+            {
+                listBox.Items.Add($"{number} - {average} = {number - average}");
+            }
+        }
+
+        public List<string> Delopgave18()
+        {
+            Random random = new Random();
+            List<int> listOfNumbers = new List<int>();
+            List<string> stringListOfNumbers = new List<string>();
+            for (int i = 0; i < 25; i++)
+            {
+                listOfNumbers.Add(random.Next(100000, 1000001));
+            }
+            int average = GetAverage(listOfNumbers);
+            listOfNumbers.Sort();
+            foreach (int number in listOfNumbers)
+            {
+                stringListOfNumbers.Add($"{number} + {average} = {number + average}");
+            }
+            return stringListOfNumbers;
+        }
+
+        public void Delopgave19(ListBox listBox)
+        {
+            ClearList(listBox);
+            Random random = new Random();
+            List<int> listOfNumbers = new List<int>();
+            for (int i = 0; i < 25; i++)
+            {
+                listOfNumbers.Add(random.Next(100000, 1000001));
+            }
+            int average = GetAverage(listOfNumbers);
+            listOfNumbers.Sort();
+            foreach (int number in listOfNumbers)
+            {
+                ListBoxItem listBoxItem = new ListBoxItem();
+                if (number % 2 == 0)
+                {
+                    listBoxItem.Background = Brushes.HotPink;
+                }
+                else
+                {
+                    listBoxItem.Background = Brushes.AliceBlue;
+                }
+                listBoxItem.Content = $"{number} - {average} = {number - average}";
+                listBox.Items.Add(listBoxItem);
+            }
+        }
+
+        public void Delopgave110(ListBox listBox)
+        {
+            ClearList(listBox);
+            Random random = new Random();
+            List<int> listOfNumbers = new List<int>();
+            List<ListBoxItem> listOfListBoxItems = new List<ListBoxItem>();
+            for (int i = 0; i < 25; i++)
+            {
+                int number = random.Next(100000, 1000001);
+                listOfNumbers.Add(number);
+            }
+            listOfNumbers.Sort();
+            int average = GetAverage(listOfNumbers);
+            foreach (int number in listOfNumbers)
+            {
+                ListBoxItem listBoxItem = new ListBoxItem();
+                if (number % 2 == 0)
+                {
+                    listBoxItem.Background = Brushes.HotPink;
+                }
+                else
+                {
+                    listBoxItem.Background = Brushes.AliceBlue;
+                }
+                listBoxItem.Content = $"{number} - {average} = {number - average}";
+                listOfListBoxItems.Add(listBoxItem);
+            }
+            listBox.ItemsSource = listOfListBoxItems;
+        }
+
+        private int GetAverage(List<int> listOfNumbers)
+        {
+            int average = 0;
+            foreach (int number in listOfNumbers)
+            {
+                average += number;
+            }
+            return average / listOfNumbers.Count;
+        }
+
+        private void ClearList(ListBox listBox)
+        {
+            if (listBox.ItemsSource != null)
+            {
+                listBox.ItemsSource = null;
+            }
+            listBox.Items.Clear();
+        }
+    }
+}
---
+++ WpfAppIntroduktion/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppIntroduktion.BIZ;
+
+namespace WpfAppIntroduktion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassBIZ classBIZ = new ClassBIZ();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearList(ListBox listBox)
+        {
+            if (listBox.ItemsSource != null)
+            {
+                listBox.ItemsSource = null;
+            }
+            listBox.Items.Clear();
+        }
+
+        private void ButtonOpg12_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave12(listBoxRes);
+        }
+
+        private void ButtonOpg13_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave13(listBoxRes);
+        }
+
+        private void ButtonOpg14_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave14(listBoxRes);
+        }
+
+        private void ButtonOpg15_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave15(listBoxRes);
+        }
+
+        private void ButtonOpg16_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave16(listBoxRes);
+        }
+
+        private void ButtonOpg17_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave17(listBoxRes);
+        }
+
+        private void ButtonOpg18_Click(object sender, RoutedEventArgs e)
+        {
+            ClearList(listBoxRes);
+            listBoxRes.ItemsSource = classBIZ.Delopgave18();
+        }
+
+        private void ButtonOpg19_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave19(listBoxRes);
+        }
+
+        private void ButtonOpg110_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Delopgave110(listBoxRes);
+        }
+    }
+}
